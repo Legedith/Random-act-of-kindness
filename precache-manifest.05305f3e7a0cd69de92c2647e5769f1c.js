01b112ce30b161554d0479e020dc040da7df671c
= <=>
2020-11-22 18:18:50 +0530

    Updates

---
+++ precache-manifest.05305f3e7a0cd69de92c2647e5769f1c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37b90fac214cd7da7749de1a3659a160",
+    "url": "/Random-act-of-kindness/index.html"
+  },
+  {
+    "revision": "0ff4b06d9ded50ef928c",
+    "url": "/Random-act-of-kindness/static/css/main.9acf2fd0.chunk.css"
+  },
+  {
+    "revision": "934565aee51d6af811cb",
+    "url": "/Random-act-of-kindness/static/js/2.b745eab0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Random-act-of-kindness/static/js/2.b745eab0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ff4b06d9ded50ef928c",
+    "url": "/Random-act-of-kindness/static/js/main.1a5e4adb.chunk.js"
+  },
+  {
+    "revision": "d215f5bcc5729a9a67dd",
+    "url": "/Random-act-of-kindness/static/js/runtime-main.a1469f3d.js"
+  },
+  {
+    "revision": "f6dd574dfe99871b9f3bfe3eb1e38ea0",
+    "url": "/Random-act-of-kindness/static/media/tumblr.f6dd574d.svg"
+  },
+  {
+    "revision": "642901af20b711c63556496841e4a8d5",
+    "url": "/Random-act-of-kindness/static/media/twitter.642901af.svg"
+  }
+]);
